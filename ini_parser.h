c74a4f4201325db0a6a30a19c7b8583fdbb7b83c
Roman <fpmk2@ya.ru>
2020-12-15 15:47:40 +0300

    initial commit

---
+++ ini_parser.h
@@ -0,0 +1,84 @@
+/* ini_parser.h
+ * Simple config file parser.
+ */
+
+#ifndef CONFIGPARSER_H
+#define CONFIGPARSER_H
+
+#include <string>
+#include <fstream>
+#include <map>
+#include <algorithm>
+
+namespace {
+// remove whitespace characters from left and right of the string
+static inline std::string trim(std::string s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+    return s;
+}
+class CfgParser {
+public:
+    CfgParser(const string& cfgFilePath) {
+        std::ifstream file(cfgFilePath.c_str());
+        if (!file.is_open()) {
+            std::cerr << "CfgParser: can\'t open config file: " << cfgFilePath << std::endl;
+            return;
+        }
+        std::string line;
+        int lineIndex = 0;
+        while (std::getline(file, line)) {
+            ++lineIndex;
+            int ioHash = line.find('#');
+            int ioEqual = line.find('=');
+            // comment line
+            if (ioHash != std::string::npos && (ioEqual == std::string::npos || ioEqual > ioHash))
+                continue;
+            // line without 'equal' sign
+            if (ioEqual == std::string::npos)
+                continue;
+            std::string keyString = trim(std::move(line.substr(0, ioEqual)));
+            std::string valString = trim(std::move(line.substr(ioEqual+1)));
+            if (keyString.empty() || valString.empty())
+                std::cerr << "Config file " << cfgFilePath << ", line #" << lineIndex << " is bad. Line:\n" << line << std::endl;
+            else
+                vals_[keyString] = valString; // latter key overwrites
+        }
+        isValid_ = true;
+    }
+
+    bool isValid() const {
+        return isValid_;
+    }
+    // returns true if config contains value
+    bool hasKey(const std::string& key) {
+        return vals_.find(key) != vals_.cend();
+    }
+    // return value by key as integer. May throw std::out_of_range
+    std::string getString(const std::string& key) const {
+        return vals_.at(key);
+    }
+    // return value by key as integer. May throw std::out_of_range and std::invalid_arg
+    int getInt(const std::string& key) const {
+        return stoi(vals_.at(key));
+    }
+    // return value by key as float. May throw std::out_of_range and std::invalid_arg
+    float getFloat(const std::string& key) const {
+        return stof(vals_.at(key));
+    }
+    // return value by key as double. May throw std::out_of_range and std::invalid_arg
+    float getDouble(const std::string& key) const {
+        return stod(vals_.at(key));
+    }
+private:
+    // values from cfg file
+    std::map<std::string, std::string> vals_;
+    bool isValid_ = false;
+};
+}
+
+#endif // CONFIGPARSER_H
